d1ac27cd575a08487aa20dffc35efcfa11548253
Akash Pisey <>
2022-11-04 05:37:54 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,26 @@
+var name = "ashish"
+let lastname = "pisey"
+let age = 32
+let fullName = "My name is: \(name) \(lastname) and my age is \(age)"
+
+//print(fullName)
+
+var price = 100
+price +=  20
+//print(price)
+
+var arr = ["Ashish", "akash"]
+arr += ["sunita"]
+//print(arr[2])
+
+for name in arr {
+  if name == "Ashish" {
+    print("hello sonu")
+  } else if name == "akash" {
+      print("hello monu")
+  } else {
+    print("hello aai")
+  }
+}
+  //print(name)
+  //print("My name is \(name)")
