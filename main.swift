6626e05f0e9d43286ff28a5d485ac1b0724d19f9
Akash Pisey <117428144+skylordreaper@users.noreply.github.com>
2022-11-08 11:26:06 +0000

    class object

--- main.swift
+++ main.swift
@@ -1,25 +1,55 @@
+class House {
+  var roomCount: Int
+  var houseName: String 
 
-func addTwoNumbers(n:Int) -> Int {
-  if n <= 1 {
-    return n
+  init(count: Int, name: String) {
+    self.roomCount = count
+    self.houseName = name
   }
-  print(n)
-  return n + addTwoNumbers(n: n - 1)
+
+  func displayHouseDetails() {
+    print("House name is \(houseName), it contains \(roomCount) number of rooms")
+  }
 }
 
-let sum = addTwoNumbers(n:5)
-print(sum)
+let piseyHouse = House(count: 6, name: "Pisey House")
+piseyHouse.displayHouseDetails()
 
-func displayName() -> String {
-   print(name)
-   return "Ashish"
+let sHouse = House(count: 4, name: "s house")
+sHouse.displayHouseDetails()
+
+class Person {
+  var name : String
+  var age : Int 
+
+  init(name: String, age: Int) {
+    self.name = name 
+    self.age = age
+  }
+
+  func displayInfo() {
+    print("Hi My name is \(name) and my age is \(age)")
+  }
 }
-let name = displayName()
-print(name)
 
-var a = 0
+class Teacher: Person {
+  var subject : String 
+  var schoolName: String 
 
-for i in 0...10 {
-  a += i
-  print(a)
+  init(subj:String, sName: String, pAge:Int, pName: String) {
+    self.subject = subj
+    self.schoolName = sName
+    super.init(name: pName, age: pAge)
+  }
+
+  func displayTeacherInfo() {
+    print("Hi I am teacher for \(subject) in \(schoolName) school")
+  }
 }
+
+// let akash = Person(name: "Akash", age: 22)
+// akash.displayInfo()
+
+let paul = Teacher(subj: "Swift", sName: "Hacking with swift", pAge: 43, pName: "Paul")
+paul.displayTeacherInfo()
+paul.displayInfo()
